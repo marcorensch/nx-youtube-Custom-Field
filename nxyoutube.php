bab4d054576bf3dad796eba7e0f3a2a056a95741
Marco <marco.rensch@area59.ch>
2017-06-21 09:06:49 +0200

    Initial Version

---
+++ nxyoutube.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @version		1.0
+ * @package		YouTube custom field for Joomla! 3.x
+ * @author		nx-designs
+ * @copyright	Copyright (C) 2017 nx-designs
+ * @license		http://opensource.org/licenses/GPL-2.0 GNU Public License, version 2.0
+ */
+
+defined( '_JEXEC' ) or die;
+
+JLoader::import('components.com_fields.libraries.fieldsplugin', JPATH_ADMINISTRATOR);
+
+/**
+ * Fields Youtube Plugin
+ *
+ * @since  3.7.0
+ */
+class PlgFieldsNxYoutube extends FieldsPlugin
+{
+}
